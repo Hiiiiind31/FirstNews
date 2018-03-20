e98854f562787ed057e54dcfc62fa2d17c3647c6
Hiiiiind_31 <hind_ahmed31@hotmail.com>
2018-03-20 13:15:18 +0200

    Initial commit

---
+++ app/src/main/java/firstnews/firstnews/Activity_Splash.java
@@ -0,0 +1,13 @@
+package firstnews.firstnews;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Activity_Splash extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity__splash);
+    }
+}
